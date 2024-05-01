ae5e091c9b4ca1ae8327ae134eed6cb55bbac390
jannayeas <janna_mattsson@hotmail.com>
2024-05-01 21:43:34 +0200

    Muteknapp i Settings
    Nu har jag gjort färdigt panelen under settings i huvudmenyn. I panelen kan man mutea/slå på bakgrundsmusiken

--- Grupp 22 Spel/Assets/Scripts/MenuSettingsController.cs
+++ Grupp 22 Spel/Assets/Scripts/MenuSettingsController.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class MenuSettingsController : MonoBehaviour
 {
+    public Sprite soundOnImage;
+    public Sprite soundOffImage;
+    public Button button;
+    private bool isOn = true;
+    public AudioSource audioSource;
 
     [SerializeField] private GameObject settingsPanel;
-    
-   public void ShowSettings()
+
+    void Start()
+    {
+        if (button != null)
+            // Tilldela knappens nuvarande sprite till soundOnImage vid start
+            soundOnImage = button.image.sprite;
+    }
+
+    public void ShowSettings()
     {
         settingsPanel.SetActive(true);
     }
@@ -15,5 +28,20 @@ public class MenuSettingsController : MonoBehaviour
     public void CloseSettings()
     {
         settingsPanel.SetActive(false);
+    }
+
+    public void ButtonClicked()
+    {
+        if(isOn)
+        {
+            button.image.sprite = soundOffImage;
+            isOn = false;
+            audioSource.mute = true;
+        } else
+        {
+            button.image.sprite = soundOnImage;
+            isOn = true;
+            audioSource.mute = false;
+        }
     }
 }
