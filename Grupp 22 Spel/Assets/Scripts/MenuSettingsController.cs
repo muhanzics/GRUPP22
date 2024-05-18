a19e761de684d720c40b06f82fde8085888bf2d0
jannayeas <janna_mattsson@hotmail.com>
2024-05-18 20:36:32 +0200

    MainMenu snart klar
    Fixat till panelen med en volymreglerare. Av någon konstig anledning funkar inte allt

--- Grupp 22 Spel/Assets/Scripts/MenuSettingsController.cs
+++ Grupp 22 Spel/Assets/Scripts/MenuSettingsController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI; 
 
@@ -10,15 +12,35 @@ public class MenuSettingsController : MonoBehaviour
     public Button button;
     private bool isOn = true;
     public AudioSource audioSource;
+    public Slider volumeSlider;
 
     [SerializeField] private GameObject settingsPanel;
 
     void Start()
-    {
+    { 
         if (button != null)
+        { 
             // Tilldela knappens nuvarande sprite till soundOnImage vid start
             soundOnImage = button.image.sprite;
     }
+
+     if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+    }
+        else
+    {
+            Load();
+    }
+
+        if (volumeSlider != null)
+    {
+        volumeSlider.onValueChanged.AddListener(delegate { changeVolume(); });
+        volumeSlider.value = AudioListener.volume; // Initialize slider with current volume
+    }
+    }
+
 
     public void ShowSettings()
     {
@@ -32,16 +54,45 @@ public class MenuSettingsController : MonoBehaviour
 
     public void ButtonClicked()
     {
-        if(isOn)
+
+        if (audioSource == null)
+        {
+            Debug.LogError("AudioSource is not assigned.");
+            return;
+        }
+
+        Debug.Log("ButtonClicked called"); // Debug-logg för felsökning
+
+        if (isOn)
         {
             button.image.sprite = soundOffImage;
             isOn = false;
             audioSource.mute = true;
+            Debug.Log("Sound turned off");
         } else
         {
             button.image.sprite = soundOnImage;
             isOn = true;
             audioSource.mute = false;
+            Debug.Log("Sound turned on");
         }
     }
-}
+
+    public void changeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+    }
+
+
