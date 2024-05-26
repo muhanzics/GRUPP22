f1de6990e241701ebb486fffe78e5669ab3628e4
muhanzics <52756077+muhanzics@users.noreply.github.com>
2024-05-26 21:18:03 +0200

    eqweqwe

--- Grupp 22 Spel/Assets/Scripts/Timer.cs
+++ Grupp 22 Spel/Assets/Scripts/Timer.cs
@@ -1,36 +1,30 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement; // Include this to manage scenes
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] float remainingTime;
-    [SerializeField] string sceneToLoad = "GameOverScene";
-    [SerializeField] GameObject pausePanel; 
+    [SerializeField] string sceneToLoad = "GameOverScene"; 
 
     void Update()
     {
-        if (pausePanel != null && pausePanel.activeSelf)
-        {
-            return; 
-        }
-
         if (remainingTime > 0)
         {
             remainingTime -= Time.deltaTime;
         }
         else
         {
-            if (remainingTime < 0)
+            if(remainingTime < 0)
             {
                 remainingTime = 0;
                 timerText.color = Color.red;
                 SceneManager.LoadScene(sceneToLoad);
             }
         }
-
         if (remainingTime > 0)
         {
             int minutes = Mathf.FloorToInt(remainingTime / 60);
